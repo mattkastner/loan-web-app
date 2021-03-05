a7a7484b74816bfb4e3f8030951af5ad61126b63
N-Smitty <66090215+N-Smitty@users.noreply.github.com>
2021-03-05 01:38:47 -0700

    lender rates css

--- src/components/LenderRates.js
+++ src/components/LenderRates.js
@@ -3,29 +3,43 @@ import "./LenderRates.scss";
 
 export default function LenderRates() {
   return (
-    <div>
+    <div className="lender-rates-container">
+      <h4 className="header-4">
+        Update your estimate with a local lenders’ estimated rate:
+      </h4>
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
+      <hr></hr>
+
       <div className="lenders">
         <div className="mortgage-company">
           <img
             className="company-logo"
-            src="./images/american-logo.png"
-            alt="american-logo"
+            src="./images/mortgage-logo.jpg"
+            alt="mortgage-logo"
           />
           <img
             className="honcho-profile"
             src="./images/profile-2.jpg"
             alt="company-honcho"
           />
-          <span className="nmls">NMLS:123456</span>
+          <div className="nmls">NMLS:123456</div>
         </div>
         <div className="company-rate">
           Rate
-          <span>2.875%</span>
-          <span>30 day rate lock</span>
+          <div className="rate-num">2.875%</div>
+          <div className="rate-lock">30 day rate lock</div>
         </div>
         <div className="rate-payment">
           Payment
-          <span>$996/mo.</span>
+          <div className="payment">$996/mo.</div>
+          {/* <div className="rate-payment">/mo.</div> */}
         </div>
         <button className="rate-button">Use Rate</button>
       </div>
@@ -34,79 +48,83 @@ export default function LenderRates() {
         <div className="mortgage-company">
           <img
             className="company-logo"
-            src="./images/american-logo.png"
-            alt="american-logo"
+            src="./images/mortgage-logo.jpg"
+            alt="mortgage-logo"
           />
           <img
             className="honcho-profile"
-            src="./images/profile-2.jpg"
+            src="./images/profile-1.jpg"
             alt="company-honcho"
           />
-          <span className="nmls">NMLS:123456</span>
+          <div className="nmls">NMLS:123456</div>
         </div>
         <div className="company-rate">
           Rate
-          <span>2.875%</span>
-          <span>30 day rate lock</span>
+          <div className="rate-num">2.875%</div>
+          <div className="rate-lock">30 day rate lock</div>
         </div>
         <div className="rate-payment">
           Payment
-          <span>$996/mo.</span>
+          <div className="payment">$996/mo.</div>
+          {/* <div className="rate-payment">/mo.</div> */}
         </div>
         <button className="rate-button">Use Rate</button>
       </div>
-
+      <hr></hr>
       <div className="lenders">
         <div className="mortgage-company">
           <img
             className="company-logo"
-            src="./images/american-logo.png"
-            alt="american-logo"
+            src="./images/mortgage-logo.jpg"
+            alt="mortgage-logo"
           />
           <img
             className="honcho-profile"
-            src="./images/profile-2.jpg"
+            src="./images/profile-3.jpg"
             alt="company-honcho"
           />
-          <span className="nmls">NMLS:123456</span>
+          <div className="nmls">NMLS:123456</div>
         </div>
         <div className="company-rate">
           Rate
-          <span>2.875%</span>
-          <span>30 day rate lock</span>
+          <div className="rate-num">2.875%</div>
+          <div className="rate-lock">30 day rate lock</div>
         </div>
         <div className="rate-payment">
           Payment
-          <span>$996/mo.</span>
+          <div className="payment">$996/mo.</div>
+          {/* <div className="rate-payment">/mo.</div> */}
         </div>
         <button className="rate-button">Use Rate</button>
       </div>
-
+      <hr></hr>
       <div className="lenders">
         <div className="mortgage-company">
           <img
             className="company-logo"
-            src="./images/american-logo.png"
-            alt="american-logo"
+            src="./images/mortgage-logo.jpg"
+            alt="mortgage-logo"
           />
           <img
             className="honcho-profile"
-            src="./images/profile-2.jpg"
+            src="./images/profile-5.jpg"
             alt="company-honcho"
           />
-          <span className="nmls">NMLS:123456</span>
+          <div className="nmls">NMLS:123456</div>
         </div>
         <div className="company-rate">
           Rate
-          <span>2.875%</span>
-          <span>30 day rate lock</span>
+          <div className="rate-num">2.875%</div>
+          <div className="rate-lock">30 day rate lock</div>
         </div>
         <div className="rate-payment">
           Payment
-          <span>$996/mo.</span>
+          <div className="payment">$996/mo.</div>
+          {/* <div className="rate-payment">/mo.</div> */}
         </div>
         <button className="rate-button">Use Rate</button>
       </div>
+      <hr></hr>
     </div>
   );
 }
