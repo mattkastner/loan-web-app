454bcb8d3bfedcc3b9259c9f687a21563e4c4573
N-Smitty <66090215+N-Smitty@users.noreply.github.com>
2021-03-08 22:47:15 -0700

    still on css

--- src/components/LenderRates.js
+++ src/components/LenderRates.js
@@ -4,9 +4,9 @@ import "./LenderRates.scss";
 export default function LenderRates() {
   return (
     <div className="lender-rates-container">
-      <h4 className="header-4">
+      <div className="header-4">
         Update your estimate with a local lenders’ estimated rate:
-      </h4>
+      </div>
       <div className="toggle-buttons">
         <label className="switch">
           <input type="checkbox" />
@@ -33,6 +33,8 @@ export default function LenderRates() {
           />
           <div className="nmls">NMLS:123456</div>
         </div>
+        <div className="company-rate-container">
+
         <div className="company-rate">
           Rate
           <div className="rate-num">2.875%</div>
@@ -41,95 +43,16 @@ export default function LenderRates() {
         <div className="rate-payment">
           Payment
           <div className="payment">$996/mo.</div>
-          {/* <div className="rate-payment">/mo.</div> */}
         </div>
         <button className="rate-button">Use Rate</button>
       </div>
+        </div>
       <hr></hr>
-      <div className="lenders">
-        <div className="mortgage-company">
-          <img
-            className="company-logo"
-            src="./images/mortgage-logo.jpg"
-            alt="mortgage-logo"
-          />
-          <img
-            className="honcho-profile"
-            src="./images/profile-1.jpg"
-            alt="company-honcho"
-          />
-          <div className="nmls">NMLS:123456</div>
-        </div>
-        <div className="company-rate">
-          Rate
-          <div className="rate-num">2.875%</div>
-          <div className="rate-lock">30 day rate lock</div>
-        </div>
-        <div className="rate-payment">
-          Payment
-          <div className="payment">$996/mo.</div>
-          {/* <div className="rate-payment">/mo.</div> */}
-        </div>
-        <button className="rate-button">Use Rate</button>
-      </div>
-      <hr></hr>
-      <div className="lenders">
-        <div className="mortgage-company">
-          <img
-            className="company-logo"
-            src="./images/mortgage-logo.jpg"
-            alt="mortgage-logo"
-          />
-          <img
-            className="honcho-profile"
-            src="./images/profile-3.jpg"
-            alt="company-honcho"
-          />
-          <div className="nmls">NMLS:123456</div>
-        </div>
-        <div className="company-rate">
-          Rate
-          <div className="rate-num">2.875%</div>
-          <div className="rate-lock">30 day rate lock</div>
-        </div>
-        <div className="rate-payment">
-          Payment
-          <div className="payment">$996/mo.</div>
-          {/* <div className="rate-payment">/mo.</div> */}
-        </div>
-        <button className="rate-button">Use Rate</button>
-      </div>
-      <hr></hr>
-      <div className="lenders">
-        <div className="mortgage-company">
-          <img
-            className="company-logo"
-            src="./images/mortgage-logo.jpg"
-            alt="mortgage-logo"
-          />
-          <img
-            className="honcho-profile"
-            src="./images/profile-5.jpg"
-            alt="company-honcho"
-          />
-          <div className="nmls">NMLS:123456</div>
-        </div>
-        <div className="company-rate">
-          Rate
-          <div className="rate-num">2.875%</div>
-          <div className="rate-lock">30 day rate lock</div>
-        </div>
-        <div className="rate-payment">
-          Payment
-          <div className="payment">$996/mo.</div>
-          {/* <div className="rate-payment">/mo.</div> */}
-        </div>
-        <button className="rate-button">Use Rate</button>
-      </div>
+
       <div className="or">- OR -</div>
-      <h4 className="header-4">
+      <div className="header-4">
         Get No-Haggle Bids, With Our Price Transparency
-      </h4>
+      </div>
       <div className="competing-button-container">
         <button className="competing-button">GET COMPETING OFFERS</button>
       </div>
