5b1eedd9b742717837676abf3df41224e8bed95d
N-Smitty <66090215+N-Smitty@users.noreply.github.com>
2021-04-26 20:18:31 -0600

    lender rates file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
-import LenderRates from "./components/LenderRates";
+import LenderRates from "./components/lenderRates/LenderRates";
 
 function App() {
-  return <div className="loan-app">
-    <LenderRates/>
-  </div>;
+  return (
+    <div className="loan-app">
+      <LenderRates />
+    </div>
+  );
 }
 
 export default App;
--- src/components/lenderRates/LenderRates.js
+++ src/components/lenderRates/LenderRates.js
@@ -8,6 +8,15 @@ export default function LenderRates() {
         Update your estimate with a local lenders’ estimated rate:
       </div>
       <div className="toggle-buttons">
+        <label class="switch">
+          <input type="checkbox" id="togBtn" />
+          <div class="slider round">
+            <span class="on">ON</span>
+            <span class="off">OFF</span>
+          </div>
+        </label>
+      </div>
+      <div className="toggle-buttons">
       <label class="switch">
  <input type="checkbox" id="togBtn"/>
  <div class="slider round">
@@ -20,6 +29,7 @@ export default function LenderRates() {
 
       <div className="lenders">
         <div className="mortgage-company">
+          <div className="mortgage-company-placeholder"></div>
           <img
             className="company-logo"
             src="./images/mortgage-logo.jpg"
@@ -33,19 +43,18 @@ export default function LenderRates() {
           <div className="nmls">NMLS:123456</div>
         </div>
         <div className="company-rate-container">
-
-        <div className="company-rate">
-          Rate
-          <div className="rate-num">2.875%</div>
-          <div className="rate-lock">30 day rate lock</div>
+          <div className="company-rate">
+            Rate
+            <div className="rate-num">2.875%</div>
+            <div className="rate-lock">30 day rate lock</div>
+          </div>
+          <div className="rate-payment">
+            Payment
+            <div className="payment">$996/mo.</div>
+          </div>
+          <button className="rate-button">Use Rate</button>
         </div>
-        <div className="rate-payment">
-          Payment
-          <div className="payment">$996/mo.</div>
-        </div>
-        <button className="rate-button">Use Rate</button>
       </div>
-        </div>
       <hr></hr>
 
       <div className="or">- OR -</div>
