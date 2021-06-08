0d396907e9b7fe36d04cbf2ca6a1e0c80c9f2af5
mattkastner <matthewkastner@gmail.com>
2021-06-08 00:04:03 -0600

    update the desktop view

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
+      <div class="loan-app__container">
         <Switch>
           <Route path="/contact-agent" component={ContactAgent} />
           <Route exact path="/" component={CalculatorCombo}>
