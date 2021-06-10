5c8e5d8a2b3d232b2fcfec2d73dbdd331befca8f
sabbir268 <sabbir.hossain2668@gmail.com>
2021-06-11 01:55:46 +0600

    Fix: pttr issue

--- backend/getdata.js
+++ backend/getdata.js
@@ -3,7 +3,9 @@ const puppeteer = require("puppeteer");
 module.exports = (req, res, next) => {
     const html = `https://docs.google.com/spreadsheets/d/e/2PACX-1vT2xjQIHvmoFTYA-vcir1VIyG3JrS61LEu4qAIbVycsBxtO4i_GYiGvigEhY7FG5AmlZR67L2qP96Z7/pubhtml`;
     (async() => {
-        const browser = await puppeteer.launch({ ignoreDefaultArgs: ['--disable-extensions'] });
+        const browser = await puppeteer.launch({
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        });
         const page = await browser.newPage();
         await page.goto(html);
 
