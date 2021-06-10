2ef32f0fa7f487389177e378ab31fa58f7c9f66e
sabbir268 <sabbir.hossain2668@gmail.com>
2021-06-10 14:48:05 +0600

    Google sheet data scrapped and send as jeson done

--- backend/getdata.js
+++ backend/getdata.js
@@ -1,82 +1,45 @@
-var request = require("request");
+const puppeteer = require("puppeteer");
 
-module.exports = function(req, res, next) {
-    try {
-        var params = req.query,
-            id = params.id,
-            sheet = params.sheet || 1,
-            query = params.q,
-            useIntegers = params.integers || true,
-            showRows = params.rows || true,
-            showColumns = params.columns || true,
-            url =
-            "https://spreadsheets.google.com/feeds/list/1KihSE1tDd_UMeaBl5fUsZmUHbYek2JpzYw1h7eoakbg/1/public/values?alt=json";
+module.exports = (req, res, next) => {
+    const html = `https://docs.google.com/spreadsheets/d/e/2PACX-1vSyYLDx48kJHM98nVOlgObsLNYYmb5CJEmjn5a3CAGLE2QJidfNL5PUdQ35Hc903W-gDdyoSZ9Yk7Sx/pubhtml`;
+    (async() => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(html);
 
-        request(url, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var data = JSON.parse(response.body);
-                var responseObj = {};
-                var rows = [];
-                var columns = {};
-                if (data && data.feed && data.feed.entry) {
-                    for (var i = 0; i < data.feed.entry.length; i++) {
-                        var entry = data.feed.entry[i];
-                        var keys = Object.keys(entry);
-                        var newRow = {};
-                        var queried = !query;
-                        for (var j = 0; j < keys.length; j++) {
-                            var gsxCheck = keys[j].indexOf("gsx$");
-                            if (gsxCheck > -1) {
-                                var key = keys[j];
-                                var name = key.substring(4);
-                                var content = entry[key];
-                                var value = content.$t;
-                                if (query) {
-                                    if (value.toLowerCase().indexOf(query.toLowerCase()) > -1) {
-                                        queried = true;
-                                    }
-                                }
-                                if (Object.keys(params).indexOf(name) > -1) {
-                                    queried = false;
-                                    if (value.toLowerCase() === params[name].toLowerCase()) {
-                                        queried = true;
-                                    }
-                                }
-                                if (useIntegers === true && !isNaN(value)) {
-                                    value = Number(value);
-                                }
-                                newRow[name] = value;
-                                if (queried === true) {
-                                    if (!columns.hasOwnProperty(name)) {
-                                        columns[name] = [];
-                                        columns[name].push(value);
-                                    } else {
-                                        columns[name].push(value);
-                                    }
-                                }
-                            }
-                        }
-                        if (queried === true) {
-                            rows.push(newRow);
-                        }
+        const result = await page.evaluate(() => {
+            const rows = document.querySelectorAll("table tr");
+            return Array.from(rows, (row) => {
+                const columns = row.querySelectorAll("td");
+                return Array.from(columns, (column) => {
+                    if (column.innerText != "") {
+                        return column.innerText;
+                    } else {
+                        const img = column.querySelectorAll("img");
+                        return Array.from(img, (im) => im.src);
                     }
-                    if (showColumns === true) {
-                        responseObj["columns"] = columns;
-                    }
-                    if (showRows === true) {
-                        responseObj["rows"] = rows;
-                    }
-                    return res.status(200).json(responseObj);
-                } else {
-                    return res.status(response.statusCode).json(error);
-                }
-            } else {
-                return res
-                    .status(200)
-                    .json("No entries returned. Do you have the right ID?");
-            }
+                });
+            });
         });
-    } catch (error) {
-        return res.status(500).json(error);
-    }
+
+        var keys = result[1];
+
+        // An array of values
+        result.splice(0, 2);
+        var values = result;
+
+        // Object created
+        var mainobj = [];
+
+        values.forEach((val) => {
+            var obj = {};
+            for (var i = 0; i < keys.length; i++) {
+                obj[keys[i].replace(" ", "")] = val[i];
+            }
+            mainobj.push(obj);
+        });
+
+        res.send(mainobj);
+        await browser.close();
+    })();
 };
